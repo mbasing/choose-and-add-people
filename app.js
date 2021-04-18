dbe0b0c81216f886fc8db27416ccde78c917d72e
Moses Basing <mosesbasing@gmail.com>
2021-04-18 17:34:53 +0000

    Design the ui

---
+++ app.js
@@ -0,0 +1,21 @@
+let app = angular.module("TossThemApp", []);
+app.controller("MovePeopleController",function($scope){
+
+    $scope.collection = [];
+    $scope.addThisOne = function(z){
+        $scope.collection.push(z);
+    }
+
+    $scope.Leaders = [
+        {"pic":"assets/1.png", "name":"Alexander"},
+        {"pic":"assets/2.png", "name":"Amanitore"},
+        {"pic":"assets/3.png", "name":"Barbarossa"},
+        {"pic":"assets/4.png", "name":"Chandragupta"},
+        {"pic":"assets/5.png", "name":"Cleoptra"},
+        {"pic":"assets/6.png", "name":"Cyrus"},
+        {"pic":"assets/7.png", "name":"Dido"},
+        {"pic":"assets/8.png", "name":"Gandhi"},
+        {"pic":"assets/9.png", "name":"Moses"},
+    ];  
+}); 
+
